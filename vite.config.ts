e8a278e048badf2b3a193e0d629ca2711079b6ab
John Fritz Izar <johnfritzizar35@gmail.com>
2026-03-12 10:06:31 +0800

    Add files via upload
    ADDED AUTHORIZATION

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify' // Add this
-import { fileURLToPath, URL } from 'node:url'
+    import { defineConfig } from 'vite'
+    import vue from '@vitejs/plugin-vue'
+    import vuetify from 'vite-plugin-vuetify' // Add this
+    import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true }), // Add this line
-  ],
-  optimizeDeps: {
-    include: ['swiper']
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  },  
-})
+    export default defineConfig({
+      plugins: [
+        vue(),
+        vuetify({ autoImport: true }), // Add this line
+      ],
+      optimizeDeps: {
+        include: ['swiper']
+      },
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+      server: {
+        port: 3000,
+        open: true
+      },
+      build: {
+        outDir: 'dist',
+        sourcemap: true
+      },  
+    })
